47d9565c71bf63f571b8be819b910211dd3060f2
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2012-08-12 23:49:02 -0400

    Make state an interface in preparation for more complexity.
    (Someone said every problem can be solved with an extra layer of
    indirection.)

--- comments.go
+++ comments.go
@@ -16,11 +16,11 @@ type reader struct {
 
 // NewReader returns an io.Reader which copies directly from r, ignoring '#' and
 // any characters following it on the same line.
-func NewReader(r io.Reader) io.Reader { return &reader{r, text, defaultDelim} }
+func NewReader(r io.Reader) io.Reader { return &reader{r, fstate(text), defaultDelim} }
 
 // NewCustomReader is identical to NewReader, except that it accepts a custom
 // delimeter
-func NewCustomReader(r io.Reader, delim byte) io.Reader { return &reader{r, text, delim} }
+func NewCustomReader(r io.Reader, delim byte) io.Reader { return &reader{r, fstate(text), delim} }
 
 func (r *reader) Read(buf []byte) (int, error) {
 	n, err := r.r.Read(buf)
@@ -28,31 +28,36 @@ func (r *reader) Read(buf []byte) (int, error) {
 	var wcount, rcount int
 	for rcount != len(buf) {
 		var written, read int
-		written, read, r.state = r.state(buf[wcount:], buf[rcount:], r.delim)
+		written, read, r.state = r.state.run(buf[wcount:], buf[rcount:], r.delim)
 		wcount += written
 		rcount += read
 	}
 	return wcount, err
 }
 
-type state func(dst, src []byte, delim byte) (written, read int, next state)
+type state interface {
+	run(dst, src []byte, delim byte) (written, read int, next state)
+}
+
+type fstate func(dst, src []byte, delim byte) (written, read int, next state)
+func (f fstate) run(dst, src []byte, delim byte) (written, read int, next state) { return f(dst, src, delim) }
 
 func text(dst, src []byte, delim byte) (written, read int, next state) {
 	for i, b := range src {
 		if b == delim {
-			return i, i + 1, comment
+			return i, i + 1, fstate(comment)
 		}
 		dst[i] = b
 	}
-	return len(src), len(src), text
+	return len(src), len(src), fstate(text)
 }
 
 func comment(dst, src []byte, delim byte) (written, read int, next state) {
 	for i, b := range src {
 		if b == '\n' {
 			dst[0] = '\n'
-			return 1, i + 1, text
+			return 1, i + 1, fstate(text)
 		}
 	}
-	return 0, len(src), comment
+	return 0, len(src), fstate(comment)
 }
