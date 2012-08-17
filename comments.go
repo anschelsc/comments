7a6be7116c5e511bcad6cade5dad33ec2354c044
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2012-08-16 21:18:59 -0400

    Rewrite comments.go to use finder package.
    Now it handles all kinds of special magic.

--- comments.go
+++ comments.go
@@ -1,49 +1,86 @@
 package comments
 
 import (
+	"bufio"
+	"github.com/anschelsc/finder"
 	"io"
 )
 
 type reader struct {
-	r io.Reader
+	r          io.ByteReader
+	begin, end *finder.Finder
+	add        []byte
 	state
 }
 
-// NewReader returns an io.Reader which copies directly from r, ignoring '#' and
-// any characters following it on the same line.
-func NewReader(r io.Reader) io.Reader { return &reader{r, text} }
-
-func (r *reader) Read(buf []byte) (int, error) {
-	n, err := r.r.Read(buf)
-	buf = buf[:n]
-	var wcount, rcount int
-	for rcount != len(buf) {
-		var written, read int
-		written, read, r.state = r.state(buf[wcount:], buf[rcount:])
-		wcount += written
-		rcount += read
+// NewCustomReader returns an io.Reader that copies directly from r, replacing
+// any string between begin and end with add.
+func NewCustomReader(r io.Reader, begin, end, add []byte) io.Reader {
+	br, ok := r.(io.ByteReader)
+	if !ok {
+		br = bufio.NewReader(r)
 	}
-	return wcount, err
+	ret := &reader{
+		r:     br,
+		begin: finder.Compile(begin),
+		end:   finder.Compile(end),
+		add:   add,
+	}
+	ret.state = text{finder.NewReader(ret.begin, ret.r)}
+	return ret
 }
 
-type state func(dst, src []byte) (written, read int, next state)
-
-func text(dst, src []byte) (written, read int, next state) {
-	for i, b := range src {
-		if b == '#' {
-			return i, i + 1, comment
-		}
-		dst[i] = b
-	}
-	return len(src), len(src), text
+// NewReader calls NewCustomReader with the appropriate arguments for parsing
+// bash-style comments.
+func NewReader(r io.Reader) io.Reader {
+	return NewCustomReader(r, []byte{'#'}, []byte{'\n'}, []byte{'\n'})
 }
 
-func comment(dst, src []byte) (written, read int, next state) {
-	for i, b := range src {
-		if b == '\n' {
-			dst[0] = '\n'
-			return 1, i + 1, text
-		}
+func (r *reader) Read(buf []byte) (n int, err error) {
+	for n != len(buf) && err == nil {
+		var nn int
+		nn, err, r.state = r.state.run(buf[n:], r)
+		n += nn
 	}
-	return 0, len(src), comment
+	return
+}
+
+type state interface {
+	run(dst []byte, up *reader) (int, error, state)
+}
+
+type text struct {
+	r io.Reader
+}
+
+func (t text) run(dst []byte, up *reader) (int, error, state) {
+	n, err := io.ReadFull(t.r, dst)
+	if err == finder.Found {
+		return n, nil, comment{finder.NewReader(up.end, up.r)}
+	}
+	return n, err, t
+}
+
+type comment struct {
+	r io.Reader
+}
+
+var cbuf [1024]byte
+
+func (c comment) run(_ []byte, up *reader) (int, error, state) {
+	_, err := io.ReadFull(c.r, cbuf[:])
+	if err == finder.Found {
+		return 0, nil, copier(up.add)
+	}
+	return 0, err, c
+}
+
+type copier []byte
+
+func (c copier) run(dst []byte, up *reader) (int, error, state) {
+	n := copy(dst, c)
+	if n == len(c) {
+		return n, nil, text{finder.NewReader(up.begin, up.r)}
+	}
+	return n, nil, c[n:]
 }
